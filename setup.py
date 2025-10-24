d6b3a2dca0987ddba3386eec547229c0ec9d161a
galibmulani <mulanigalib1@gmail.com>
2025-10-24 22:58:28 +0530

    setup script coded

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(filepath:str)->List[str]:
+    requirements = []
+    with open(filepath) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n','') for req in requirements]
+
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='Xray',
+    version='0.0.1',
+    author='Mulani Galib',
+    author_email='mulanigalib1@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements("C:\\Users\\Lenovo\\deeplearningproject\\requirements_dev.txt"),
+)
